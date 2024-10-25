959073a9073179d8fdba047ce1ce1c96bbd14939
CRVanware <crodriguez@vanware.io>
2024-10-25 18:30:35 +0000

    algorito v1.0

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let ext = ['.com', '.net', '.io']
+
+for(let p = 0; p < pronoun.length; p++) {
+    for(let a = 0; a < adj.length; a++) {
+      for(let n = 0; n < noun.length; n++) {
+        for(let e = 0; e < ext.length; e++) {
+        console.log(pronoun[p] + adj[a] + noun[n]+ ext[e]); 
+        }
+      }
+    }
+}
